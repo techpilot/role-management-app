5b7c1def8cbeb72332d7fbf48ccab9a996ad6b05
techpilot <ngwustephen99@gmail.com>
2023-12-10 16:54:00 +0100

    fetched user data json and populated them in the users table

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,4 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface UserDataState {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  ip_address: string;
+  friends: [
+    {
+      id: string;
+      name: string;
+    }
+  ];
+}
+
 const UserTable = () => {
+  const [users, setUsers] = useState<UserDataState[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get("/users.json");
+      console.log(response);
+      setUsers(response?.data);
+    })();
+  }, []);
+  console.log(users);
+
   return (
     <div className="w-full">
       <div className="grid grid-cols-7 items-center text-[0.5rem] lg:text-xs bg-gray-50 p-2 lg:p-5 gap-28 md:gap-5 text-[#636872]">
@@ -11,15 +40,24 @@ const UserTable = () => {
         <p className="text-left font-semibold min-w-max">Friends</p>
       </div>
 
-      <div className="grid grid-cols-7 items-center text-[0.5rem] lg:text-[0.75rem] mb-[0.5rem] p-2 lg:p-5 gap-28 md:gap-5">
-        <p className="text-left">1</p>
-        <p className="text-left">Ngwu</p>
-        <p className="text-left">Stephen</p>
-        <p className="text-left">ngwustephen99@gmail.com</p>
-        <p className="text-left">Male</p>
-        <p className="text-left">129.382</p>
-        <p className="text-left">Brothers</p>
-      </div>
+      {users?.map((user, index) => (
+        <div
+          key={index}
+          className="grid grid-cols-7 items-center text-[0.5rem] lg:text-[0.75rem] mb-[0.5rem] p-2 lg:p-5 gap-28 md:gap-5"
+        >
+          <p className="text-left">{user.id}</p>
+          <p className="text-left">{user.first_name}</p>
+          <p className="text-left">{user.last_name}</p>
+          <p className="text-left">{user.email}</p>
+          <p className="text-left">{user.gender}</p>
+          <p className="text-left">{user.ip_address}</p>
+          <p className="text-left">
+            {user.friends.map((friend) => (
+              <>{`${friend.name}, `}</>
+            ))}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
