7f52f6cc36c5caf28d776c45ab277ad8b1dab5a7
techpilot <ngwustephen99@gmail.com>
2023-12-10 15:30:21 +0100

    added users json data

--- src/components/UserContainer.tsx
+++ src/components/UserContainer.tsx
@@ -1,13 +1,10 @@
 const UserContainer = () => {
   return (
-    <div className="w-[100vw] flex flex-col justify-between h-full px-[1rem] relative">
+    <div className="w-[100vw] flex flex-col justify-between h-full px-[0.5rem] relative">
       <div className="w-full h-[20rem] bg-[#8E0789] absolute left-0 -z-10 m-auto"></div>
-      <div className="h-[85vh] mt-[4rem] flex flex-col lg:flex-row items-start gap-7 mx-[3rem]">
-        <div
-          // onSubmit={handleSubmit(onSubmit)}
-          className="flex-1 lg:h-full w-full bg-white rounded-lg shadow-lg overflow-y-scroll no-scrollbar p-5 xl:p-10 flex flex-col gap-3 md:gap-[3rem]"
-        >
-          user data
+      <div className="h-[95vh] lg:h-[85vh] mt-[1rem] md:mt-[4rem] flex flex-col lg:flex-row items-start gap-7 mx-0 lg:mx-[3rem]">
+        <div className="flex-1 lg:h-full w-full bg-white rounded-sm shadow-lg overflow-scroll no-scrollbar p-5 flex flex-col gap-3 md:gap-[3rem]">
+          <p className="text-2xl font-semibold text-[#8E0789]">Users</p>
         </div>
       </div>
     </div>
